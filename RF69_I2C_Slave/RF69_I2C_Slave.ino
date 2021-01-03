2833a44dfbaeed1ad796bf299e0c929a48119e19
infrapale <infrapale@gmail.com>
2021-01-03 11:52:14 +0200

    recevie via radio anf fwd to i2c, in testing

--- RF69_I2C_Slave/RF69_I2C_Slave.ino
+++ RF69_I2C_Slave/RF69_I2C_Slave.ino
@@ -42,6 +42,7 @@
 #define RFM69_RX_AVAIL    0x40
 #define RFM69_RX_LOAD_MSG 0x41
 #define RFM69_RX_RD_MSG   0x42
+#define RFM69_RX_RD_LEN   0x43
 #define RFM69_TX_FREE     0x50
 
 
@@ -59,8 +60,16 @@ uint8_t tx_buf[RFM69_BUF_LEN];
 
 uint8_t tx_sema;
 uint8_t rx_sema;
-uint8_t i2c_request_command;
 
+struct i2c_mgmt_struct{
+    uint8_t request_command;
+    uint8_t rd_len;
+    uint8_t rx_bytes;
+    uint8_t offset;
+    
+};
+
+i2c_mgmt_struct i2c_mgmt;
 
 
 enum key_states {
@@ -89,7 +98,9 @@ void setup() {
     Serial.begin(9600);
     Serial.println("RF69 I2C_Slave Tom Höglund 2020");
 
-    i2c_request_command = 0x00;
+    i2c_mgmt.request_command = 0x00;
+    i2c_mgmt.rd_len = 0x00;
+    i2c_mgmt.offset = 0x00;
     
     Wire.begin(RFM_I2C_ADDR);      // join i2c bus with address 
     Wire.onRequest(RequestEvent);  // register event
@@ -138,17 +149,18 @@ void ReceiveEvent(int howMany)
           break;
         }
     }
-    buf_len = idx; 
-    if (buf_len == 1) i2c_request_command = i2c_event_buf[0];
-    else i2c_request_command = 0x00;
-    /*
-    Serial.print("receive event  buf[0]=");
-    Serial.print(i2c_event_buf[0]);
-    Serial.print("  buf[1]=");
-    Serial.print(i2c_event_buf[1]);
-    Serial.print("  buf_len=");
-    Serial.println(buf_len);
-    */
+    buf_len = idx;
+    memset(&i2c_mgmt,0x00,sizeof(i2c_mgmt)); 
+    if (buf_len > 0) i2c_mgmt.request_command = i2c_event_buf[0];
+    if (buf_len > 1) i2c_mgmt.offset          = i2c_event_buf[1];
+    if (buf_len > 2) i2c_mgmt.rd_len          = i2c_event_buf[2];
+    
+    
+    Serial.print("request_command="); Serial.print(i2c_mgmt.request_command);
+    Serial.print(", offset =");  Serial.print(i2c_mgmt.offset);
+    Serial.print(", rd_len =");  Serial.print(i2c_mgmt.rd_len);
+    Serial.print(",  buf_len="); Serial.println(buf_len);
+    
     switch (i2c_event_buf[0]) {
     case RFM69_RESET:
         radio_init(RFM69_CS,RFM69_INT,RFM69_RST, RFM69_FREQ);
@@ -209,8 +221,12 @@ void ReceiveEvent(int howMany)
         break;
 
     case RFM69_RX_LOAD_MSG:
-        rx_len =  RxData.GetArray(i2c_load_buf, RFM69_BUF_LEN)
-        memcpy( i2c_load_buf[buf_pos] , &i2c_event_buf[2], buf_len-2);
+        if (RxData.Available() > 0){
+            i2c_mgmt.rx_bytes =  RxData.GetArray(i2c_load_buf, RFM69_BUF_LEN);
+        }
+        else {
+            i2c_mgmt.rd_len = 0;
+        }
         break;
     }
 }
@@ -227,9 +243,11 @@ void ReceiveEvent(int howMany)
 void RequestEvent()
 {
    uint8_t buf[2];
+   uint8_t n_bytes;
+   uint8_t indx;
    
-   Serial.print("RequestEvent  cmd = ");  Serial.println(i2c_request_command,HEX);
-   switch (i2c_request_command){
+   Serial.print("RequestEvent  cmd = ");  Serial.println(i2c_mgmt.request_command,HEX);
+   switch (i2c_mgmt.request_command){
    case RFM69_RX_AVAIL:
        buf[0] = RxData.Available();
        Wire.write(buf,1);
@@ -238,10 +256,31 @@ void RequestEvent()
        buf[0] = TxData.Free();
        Wire.write(buf,1);
        break;
+    case RFM69_RX_RD_MSG:
+        if ((i2c_mgmt.offset + i2c_mgmt.rd_len) > RFM69_BUF_LEN-1){
+            if (i2c_mgmt.offset > RFM69_BUF_LEN){
+                n_bytes = 0;
+            } else {
+                n_bytes =  RFM69_BUF_LEN - i2c_mgmt.offset;         
+            }
+        } else {
+            n_bytes = i2c_mgmt.rd_len;
+        }
+        Wire.write(&i2c_load_buf[i2c_mgmt.offset],i2c_mgmt.rd_len);
+        //for (indx = i2c_mgmt.offset; indx < i2c_mgmt.offset + i2c_mgmt.rd_len; indx++){
+        //     
+        //}
+        
+        break;  
+    case  RFM69_RX_RD_LEN:
+        buf[0] = i2c_mgmt.rx_bytes;
+        Wire.write(buf,1);
+        break;      
     default:
        buf[0] = 0xAA;
        buf[1] = 0x55;
-       Wire.write(buf,1);    
+       Wire.write(buf,1);
+       break;    
     }
 }
 
@@ -274,7 +313,7 @@ void radio_rx_handler(void){
         len = radio_read_msg(json_msg, RFM69_BUF_LEN);
         for (i=0;i<len;i++) Serial.print(json_msg[i]);
         Serial.println();  
-        if (RxData.Free > 0){
+        if (RxData.Free() > 0){
             RxData.AddArray(json_msg,RFM69_BUF_LEN);             
         } else {
             Serial.println("Rx buffers are full, message not added");
